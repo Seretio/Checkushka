fc77eb1d0ad4caafadaaed40048425f72706ea9c
Seretio <polinkorrt@inbox.lv>
2026-09-19 11:39:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from aiogram.types import (
 )
 
 # === НАСТРОЙКИ ИЗ ПЕРЕМЕННЫХ ОКРУЖЕНИЯ ===
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8950292427:AAFSAxFmSoc9VLv_DAxWifXr8IfVQ7zLIp8")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8950292427:AAFYyNCxkhhkVfaobZEnNWYgWoSz_LpkSiI")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "Checkushhka_Bot")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
