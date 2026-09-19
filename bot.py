40cf2df3702be84a70ccdf3438cc0ca7d830cbbd
Seretio <polinkorrt@inbox.lv>
2026-09-19 11:24:10 +0300

    Change BOT_TOKEN in bot.py
    Updated the BOT_TOKEN environment variable.

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from aiogram.types import (
 )
 
 # === НАСТРОЙКИ ИЗ ПЕРЕМЕННЫХ ОКРУЖЕНИЯ ===
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8950292427:AAHiJ26IAGA4cTwC4OAnJU3DxZUVE8Ld7xg")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8950292427:AAFubqblK31adjliqCw-rIVb5gMLnAGp0RA")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "Checkushhka_Bot")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
